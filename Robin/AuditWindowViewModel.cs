2f5da6f05dea4e100835f5cf4c12c829acc122a2
majormajor0 <mtpogue@gmail.com>
2018-03-20 16:58:36 -0700

    Add audit window

---
+++ Robin/AuditWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robin
+{
+	public class AuditWindowViewModel : INotifyPropertyChanged
+	{
+		List<MAME.AuditResult> auditResults;
+		public List<MAME.AuditResult> AuditResults
+		{
+			get
+			{
+				return auditResults;
+			}
+
+			set
+			{
+				auditResults = value;
+				OnPropertyChanged("AuditResults");
+				OnPropertyChanged("GoodResults");
+				OnPropertyChanged("BadResults");
+				OnPropertyChanged("BestResults");
+				OnPropertyChanged("GoodCount");
+				OnPropertyChanged("BadCount");
+				OnPropertyChanged("BestCount");
+			}
+		}
+
+		public List<MAME.AuditResult> GoodResults => AuditResults?.Where(x => x.Result.ToLower() == "good").ToList();
+
+		public List<MAME.AuditResult> BadResults => AuditResults?.Where(x => x.Result.ToLower() == "bad").ToList();
+
+		public List<MAME.AuditResult> BestResults => AuditResults?.Where(x => x.Result.ToLower() == "best").ToList();
+
+		public int GoodCount => AuditResults?.Count(x => x.Result.ToLower() == "good") ?? 0;
+
+		public int BadCount => AuditResults?.Count(x => x.Result.ToLower() == "bad") ?? 0;
+
+		public int BestCount => AuditResults?.Count(x => x.Result.ToLower() == "best") ?? 0;
+
+		public AuditWindowViewModel()
+		{
+			GetAuditResults();
+		}
+
+		async void GetAuditResults()
+		{
+			await Task.Run(() => { AuditResults = MAME.AuditRoms(); });
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged(string prop)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+		}
+	}
+}
